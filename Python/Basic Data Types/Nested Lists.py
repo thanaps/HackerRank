822c653346f45958256d16762334174fb0022299
thanaps <119058036+thanaps@users.noreply.github.com>
2023-10-06 14:25:35 +0700

    Create Nested Lists.py

---
+++ Python/Basic Data Types/Nested Lists.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        Result.append([name,score])
+        scorelist += [score]
+    second_lowest =sorted(list(set(scorelist)))[1] 
+    for firstname, score_own in sorted(Result):
+        if score_own == second_lowest:
+            print(firstname)
