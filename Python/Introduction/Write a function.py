bef9015741f10389c27be34d9669c6daf7c08744
thanaps <119058036+thanaps@users.noreply.github.com>
2023-10-06 11:26:54 +0700

    Update Write a function.py

--- Python/Introduction/Write a function.py
+++ Python/Introduction/Write a function.py
@@ -3,6 +3,7 @@ def is_leap(year):
     
     # Write your logic here
     if year % 4 == 0:
+        leap = True
         if year % 100 == 0 and year % 400 != 0:
          leap = False
     return leap
